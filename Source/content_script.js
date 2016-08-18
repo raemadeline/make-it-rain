4b747b7216ad801c26f3b4018142e3324e217f8c
raemadeline <maddie.horowitz@addepar.com>
2016-08-18 14:31:20 -0400

    :money_with_wings::money_with_wings::money_with_wings:

---
+++ Source/content_script.js
@@ -0,0 +1,102 @@
+width = null
+height = null
+imageHeight = 100
+fallingMoney = []
+canvasContext = null
+interval = null
+
+width = $(document).width()
+height = $(document).height()
+canvas = $('<canvas class="rain"></canvas>')
+canvas.attr('width', width)
+canvas.attr('height', height)
+canvas.appendTo('body')
+initAnimation()
+
+function initAnimation() {
+  numMoney = 3//300
+  speedOffset = 10
+  speedRange = 5
+  numImages = 6
+  frameRate = 1000 / 30 // 30 frames per second
+  animationLength = 10000 // 10 seconds
+
+  canvasContext = $('.rain')[0].getContext('2d')
+
+  for(var index = 0; index < numMoney; index++) {
+
+    isOdd = index % 2 == 1
+    direction = 0;
+    if(isOdd)
+      direction = 1;
+    else
+      direction = -1;
+
+    money = {
+      image: new Image(),
+      x: _random(width),
+      y: _random(-height * 2, -imageHeight),
+      angle: _random(2 * Math.PI),
+      speed: speedOffset + _random(speedRange),
+      currentFrame: 0,
+      direction: direction
+    }
+
+    imageIndex = _random(numImages)
+    imagePath = "images/money_" + imageIndex + ".png"
+    debugger
+    imageURL = chrome.extension.getURL(imagePath);
+    money.image.src = imageURL
+    fallingMoney.push(money)
+  }
+
+  interval = setInterval(function() {
+    draw()
+  }, frameRate)
+
+  setTimeout(function() {
+    endAnimation()
+  }, animationLength)
+}
+
+function draw() {
+  clearWindow()
+
+  fallingMoney.forEach(function(money, index) {
+    drawRotatedImage(money)
+
+    money.currentFrame += 1
+    money.y += money.speed
+    money.angle += money.direction * 0.1
+    radius = money.direction * (10 + (index % 6))
+    money.x += Math.sin((money.currentFrame + index) / (2 * Math.PI)) * radius
+  })
+}
+
+function clearWindow() {
+  canvasContext.clearRect(0, 0, width, height)
+}
+
+function drawRotatedImage(money) {
+  canvasContext.save()
+  canvasContext.translate(money.x, money.y)
+  canvasContext.rotate(money.angle)
+  canvasContext.drawImage(money.image, 0, 0)
+  canvasContext.restore()
+}
+
+function endAnimation() {
+  clearInterval(interval)
+  fallingMoney = []
+  canvas.detach()
+}
+
+function _random(lowVal, highVal) {
+  if (highVal === undefined) {
+    highVal = lowVal
+    lowVal = 0
+  }
+
+  randVal = Math.floor(Math.random() * (highVal - lowVal)) + lowVal
+  return randVal
+}
